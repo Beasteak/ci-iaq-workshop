8aa47ea9a66d85577e6f2df56eed86a220d37d9b
Gerhard Kofler <gerhard.kofler@stiwa.com>
2021-06-09 14:33:57 +0200

    Create Vm

--- ci-test-vm/main.tf
+++ ci-test-vm/main.tf
@@ -5,7 +5,7 @@ provider "hcloud" {
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "ansible-test-ssh-key" {
-  name = "Ansible Test VM SSH Key"
+  name = "Ansible Test VM SSH Key Gerhard"
   public_key = file(var.ssh_key)
 }
 
@@ -16,7 +16,7 @@ resource "hcloud_server" "ansible-test-vm" {
   server_type = "cx11"
   location = "nbg1"
   ssh_keys = [
-    "Ansible Test VM SSH Key"
+    "Ansible Test VM SSH Key Gerhard"
   ]
 
   provisioner "local-exec" {
